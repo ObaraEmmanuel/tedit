1ca2dc8c20191f72a243c06859a73166b8f3cc24
ObaraEmmanuel <emmanuelobarany@gmail.com>
2021-05-20 16:03:34 +0300

    Implement line numbers

--- tedit/editor.py
+++ tedit/editor.py
@@ -1,6 +1,6 @@
 from tkinter import Frame, ttk
 
-from tedit.text import TextArea
+from tedit.text import TextArea, LineNumbers
 
 
 class Editor(Frame):
@@ -8,30 +8,34 @@ class Editor(Frame):
     def __init__(self, master, **kw):
         super().__init__(master, **kw)
         self.gutter = Frame(self, bg="#353535")
-        self.text = TextArea(
-            self, wrap='none', relief='flat', bd=0, bg="#202020",
-            highlightthickness=0, fg="#f7f7f7", padx=5, pady=5)
-        self.text.bind("<<TextChange>>", lambda e: print(self.text.get_all()))
+        self.text = TextArea(self)
+        self.text.bind("<<TextChange>>", self._on_change)
         self.x_scroll = ttk.Scrollbar(self, orient="horizontal")
         self.y_scroll = ttk.Scrollbar(self, orient="vertical")
-        self.gutter.grid(row=0, column=0, rowspan=2, sticky='nswe')
+        self.gutter.grid(row=0, column=0, sticky='nswe')
+        self.line_numbers = LineNumbers(self.gutter)
+        self.line_numbers.pack(fill="both", expand=True)
         self.text.grid(row=0, column=1)
         self.y_scroll.grid(row=0, column=2, sticky='ns')
         self.x_scroll.grid(row=1, column=1, sticky='ew')
         self.columnconfigure(1, uniform=1)
-        self.columnconfigure(0, minsize=30)
         self.text.configure(
             yscrollcommand=self._set_scroll_y,
             xscrollcommand=self._set_scroll_x
         )
+        self.line_numbers.configure(yscrollcommand=self._set_line_number_scroll)
         self.x_scroll.configure(command=self._scroll_x)
         self.y_scroll.configure(command=self._scroll_y)
+
+    def _on_change(self, _):
+        self.line_numbers.update_numbers(self.text)
 
     def _scroll_x(self, *args):
         self.text.xview(*args)
 
     def _scroll_y(self, *args):
         self.text.yview(*args)
+        self.line_numbers.yview(*args)
 
     def _set_scroll_x(self, *args):
         if args == ('0.0', '1.0'):
@@ -47,4 +51,13 @@ class Editor(Frame):
                 self.y_scroll.grid_forget()
         elif not self.y_scroll.winfo_ismapped():
             self.y_scroll.grid(row=0, column=2, sticky='ns')
+
+        if self.line_numbers.yview() != self.text.yview():
+            self.line_numbers.yview_moveto(args[0])
         self.y_scroll.set(*args)
+
+    def _set_line_number_scroll(self, *args):
+        if self.text.yview() != self.line_numbers.yview():
+            self.text.yview_moveto(args[0])
+        self.y_scroll.set(*args)
+
--- tedit/text.py
+++ tedit/text.py
@@ -4,7 +4,13 @@ import tkinter as tk
 class TextArea(tk.Text):
     
     def __init__(self, master, **kw):
-        cnf = {"undo": True, "maxundo": -1, "autoseparators": True, **kw}
+        cnf = dict(
+            wrap=tk.NONE, relief=tk.FLAT, bd=0, bg="#202020",
+            highlightthickness=0, fg="#f7f7f7", padx=5, pady=5,
+            undo=True, maxundo=-1, autoseparators=True,
+            insertbackground="#f7f7f7",
+        )
+        cnf.update(kw)
         super(TextArea, self).__init__(master, cnf)
         self.bind("<<Modified>>", self._on_change)
         self.bind("<Control-z>", lambda _: self.edit_undo())
@@ -36,3 +42,43 @@ class TextArea(tk.Text):
             pass
         # suppress any external bindings
         return "break"
+
+
+class LineNumbers(tk.Text):
+
+    def __init__(self, master, **kw):
+        # defaults
+        cnf = dict(
+            bg="#353535", fg="#aaaaaa", width=3, relief=tk.FLAT, bd=0,
+            highlightthickness=0, padx=5, pady=5, wrap=tk.NONE,
+            selectbackground="#353535", selectforeground="#aaaaaa",
+            exportselection=0, insertbackground="#353535",
+            inactiveselectbackground="#353535", takefocus=False
+        )
+        cnf.update(kw, state=tk.DISABLED)
+        super(LineNumbers, self).__init__(master, **cnf)
+        self.insert(tk.END, "1")
+        self._last = 1
+
+    def insert(self, index, chars, *args):
+        self.configure(state=tk.NORMAL)
+        super(LineNumbers, self).insert(index, chars, *args)
+        self.configure(state=tk.DISABLED)
+
+    def delete(self, index1, index2=None):
+        self.configure(state=tk.NORMAL)
+        super(LineNumbers, self).delete(index1, index2)
+        self.configure(state=tk.DISABLED)
+
+    def update_numbers(self, text):
+        line, _ = list(map(int, text.index(tk.END).split(".")))
+        last, _ = list(map(int, self.index(tk.END).split(".")))
+        if line == last:
+            return
+
+        if line > last:
+            self.insert(tk.END, "\n")
+            self.insert(tk.END, "\n".join(list(map(str, range(last, line)))))
+        elif last > line:
+            self.delete("{}.0".format(line), tk.END)
+        self.configure(width=max(3, len(str(line - 1))))
